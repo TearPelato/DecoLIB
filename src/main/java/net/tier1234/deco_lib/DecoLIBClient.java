a277101e5d493961b23c52393142aa0a7d7641b9
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-02-01 17:09:06 +0100

    Refactored Base class

--- src/main/java/net/tier1234/deco_lib/DecoLIBClient.java
+++ src/main/java/net/tier1234/deco_lib/DecoLIBClient.java
@@ -1,4 +1,18 @@
 package net.tier1234.deco_lib;
 
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod(value = DecoLIB.MOD_ID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = DecoLIB.MOD_ID, value = Dist.CLIENT)
 public class DecoLIBClient {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+
+
 }
