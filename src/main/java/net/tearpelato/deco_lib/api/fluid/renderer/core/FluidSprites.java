777a62bc6551160d6910e4efbbd2e0f9676ffdf2
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-04-07 14:14:25 +0200

    Declaring FluidSprites contents to prevent null crash

--- src/main/java/net/tearpelato/deco_lib/api/fluid/renderer/core/FluidSprites.java
+++ src/main/java/net/tearpelato/deco_lib/api/fluid/renderer/core/FluidSprites.java
@@ -1,10 +1,18 @@
 package net.tearpelato.deco_lib.api.fluid.renderer.core;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.FluidModel;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.world.level.material.FluidState;
 
 /**
  * @author MrCrayfish*/
 
 public record FluidSprites(TextureAtlasSprite still, TextureAtlasSprite flowing)
 {
+    public FluidSprites getFluidSprites(FluidState state)
+    {
+        FluidModel model = Minecraft.getInstance().getModelManager().getFluidStateModelSet().get(state);
+        return new FluidSprites(model.stillMaterial().sprite(), model.flowingMaterial().sprite());
+    }
 }
